025f6b4fdd160edba78ad27de3ee3ac19740c943
Chad Parr <cparr2@css.edu>
2016-01-27 18:49:34 -0600

    Initial Commit

---
+++ Mr. Fox and Turtle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mr. Fox and Turtle
+//
+//  Created by chad parr on 1/27/16.
+//  Copyright © 2016 chad parr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
